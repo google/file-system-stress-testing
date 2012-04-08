8cea7ad50594097485beafcd50a13dcdd6e55c50
Kirk Russell <kirussel@users.noreply.github.com>
2012-04-08 18:53:13 +0000

    Added a new header to help with linux porting.

---
+++ src/lib/bstg.h
@@ -0,0 +1,34 @@
+#ifndef __BSTG_H
+#define __BSTG_H
+
+/*
+ * Copyright 2011 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * $Id: $
+ */
+
+#ifndef __RCSID
+#include <unistd.h>
+#include <sys/cdefs.h>
+#endif
+
+#ifndef __RCSID
+#define __RCSID(__not_used) struct rcsid_not_defined
+#endif
+
+#ifdef linux
+#include <bsd/stdlib.h>
+#endif
+
+#endif
